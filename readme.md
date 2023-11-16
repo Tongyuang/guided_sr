55a4c0e237141a7a7abf0fef39be5b0f7cb187a5
yuang <yuangtong1999@gmail.com>
2023-11-16 22:51:48 +0000

    update readme, test images and baselines

---
+++ readme.md
@@ -0,0 +1,5 @@
+Guided SR
+======
+Approach to guided SR to remote high resolution image transmission.
+
+Remote high resolution image -> comressed low resolution image + feature -> transmission -> reconstruction through SR
---
+++ test_images/Farm_image/image.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   image.py
+@Time    :   2023/11/16 14:40:13
+@Author  :   Yuang Tong 
+@Contact :   yuangtong1999@gmail.com
+'''
+
+# process default images
+
+# here put the import lib
